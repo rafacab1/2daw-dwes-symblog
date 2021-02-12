035238f50660edbc3a7f1a0daff6d85c2a2f51b0
Rafa Caballero <rafacab.dev@gmail.com>
2021-02-12 11:06:28 +0100

    Tag cloud y latest comments

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -2,6 +2,8 @@
 namespace App\Controllers;
 
 use Laminas\Diactoros\Response\HtmlResponse;
+use App\Models\Blog;
+use App\Models\Comment;
 
 class BaseController {
     protected $templateEngine;
@@ -15,11 +17,33 @@ class BaseController {
     }
 
     public function render($filename, $data=[]) {
+        // Tags
+        $blogs = Blog::all();
+        $tags = [];
+        foreach ($blogs as $blog) {
+            $tags = array_merge($tags, explode(",", $blog->tags));
+        }
+
+        // Recent comments
+        $allComments = Comment::all();
+        $comments = [];
+        $counter = 0;
+        foreach ($allComments as $comment) {
+            array_push($comments, $comment);
+            $counter++;
+            if ($counter >= 5) {
+                // Limit to 5 comments
+                break;
+            }
+        }
+
+        // User loged
         $loged = null;
         if (isset($_SESSION['user'])) {
             $loged = $_SESSION['user'];
         }
-        $data = array_merge($data, array("user" => $loged));
+
+        $data = array_merge($data, array("user" => $loged), array("tags" => $tags), array("latestComments" => $comments));
         return new HtmlResponse($this->templateEngine->render($filename, $data));
     }
 }
